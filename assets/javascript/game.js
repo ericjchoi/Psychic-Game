968bcc43e90c060b07fea51ca3b434460b2a4712
Eric Choi <39499807+ericjchoi@users.noreply.github.com>
2019-09-14 13:00:21 -0700

    new branch

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,13 +3,16 @@
 /* 11th user typing resets the browser */
 /* game resumes from 12th typing */
 
-var computerLetterSet = ["a", "b", "c", "d", "e", "f",
-    "g", "h", "i", "j", "k", "l", "m", "n", "o", "p",
-    "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+// var computerLetterSet = ["a", "b", "c", "d", "e", "f",
+//     "g", "h", "i", "j", "k", "l", "m", "n", "o", "p",
+//     "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+var computerLetterSet = ["a", "b"]; /* testing array */
 
 var winNum = 0;
 var loseNum = 0;
-var guessLeftNum = 10;
+var totalNumGuessAllowed = 10;
+var guessLeftNum;
 var guessSoFar = [];
 var userKey = "";
 var i = 0;
@@ -18,7 +21,7 @@ var j = 0;
 function initialize() {
     winNum = 0;
     loseNum = 0;
-    guessLeftNum = 10;
+    guessLeftNum = totalNumGuessAllowed;
     guessSoFar = [];
     userKey = "";
     i = 0;
@@ -27,25 +30,26 @@ function initialize() {
 function initialDisplay() {
     document.getElementById("winNum").innerHTML = 0;
     document.getElementById("loseNum").innerHTML = 0;
-    document.getElementById("guessLeftNum").innerHTML = 10;
+    document.getElementById("guessLeftNum").innerHTML = totalNumGuessAllowed;
     document.getElementById("guessSoFar").innerHTML = "";
 }
+
 initialize();
 initialDisplay();
 
-document.onkeyup = function (event) {
+document.onkeypress = function (event) {
 
     var userKey = event.key.toLowerCase();
     guessSoFar[i] = userKey;
 
-    var computerRandNum = Math.floor(Math.random() * 26);
+    var computerRandNum = Math.floor(Math.random() * computerLetterSet.length);
     var x = computerLetterSet[computerRandNum];
 
     if (x === guessSoFar[i]) {
         document.getElementById("winNum").innerHTML = ++winNum;
     }
     else {
-        document.getElementById("loseNum").innerHTML = ++loseNum;
+        // document.getElementById("loseNum").innerHTML = ++loseNum;
     }
 
     document.getElementById("guessLeftNum").innerHTML = --guessLeftNum;
@@ -56,7 +60,7 @@ document.onkeyup = function (event) {
     i++;
 
     /* user has 10 times of chance */
-    if (i === 11) {
+    if (i === totalNumGuessAllowed + 1) {
         location.reload();
     }
 }
