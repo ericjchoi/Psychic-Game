db423e5a48d070d6fac18cb34bef0bc8fd408187
Eric Choi <39499807+ericjchoi@users.noreply.github.com>
2019-09-17 14:50:39 -0700

    HW3 Psychic Game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,25 +1,8 @@
 /* HW3 Psychic Game */
-
-/* HOW to update code into other branch
-   update your code in vscode
-   in terminal ....
-   git branch
-   git checkout -b version2
-   git add .
-   git commit -m "version2 update"
-   git push origin version2
-*/
-
-/* User has 10 times of chance to guess letters */
-/* 11th user typing resets the browser */
-/* game resumes from 12th typing */
-
-// var computerLetterSet = ["a", "b", "c", "d", "e", "f",
-//     "g", "h", "i", "j", "k", "l", "m", "n", "o", "p",
-//     "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-
-var computerLetterSet = ["a", "b", "c", "d"]; /* testing array */
-
+/* set up global variables and arrays */
+var computerLetterSet = ["a", "b", "c", "d", "e", "f",
+    "g", "h", "i", "j", "k", "l", "m", "n", "o", "p",
+    "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var winNum = 0;
 var loseNum = 0;
 var totalNumGuessAllowed = 10;
@@ -28,7 +11,7 @@ var guessSoFar = [];
 var userKey = "";
 var i = 0;
 var j = 0;
-
+/* functions */
 function initialize() {
     guessLeftNum = totalNumGuessAllowed;
     guessSoFar = [];
@@ -42,31 +25,32 @@ function initialDisplay() {
     document.getElementById("guessLeftNum").innerHTML = totalNumGuessAllowed;
     document.getElementById("guessSoFar").innerHTML = "";
 }
-
+/* game started for the first time */
 initialize();
 initialDisplay();
-
+/* key pressing events in game */
 document.onkeypress = function (event) {
-
+    /* user presses a key */
     var userKey = event.key.toLowerCase();
     guessSoFar[i] = userKey;
-
+    /* computer decides a random alphabet letter */
     var computerRandNum = Math.floor(Math.random() * computerLetterSet.length);
     var x = computerLetterSet[computerRandNum];
-
+    /* user won the game */
     if (x === guessSoFar[i]) {
         document.getElementById("winNum").innerHTML = ++winNum;
         initialize();
         initialDisplay();
         document.getElementById("guessLeftNum").innerHTML = guessLeftNum;
     }
+    /* user keeps playing before win or lose decided */
     else {
         document.getElementById("guessLeftNum").innerHTML = --guessLeftNum;
         var y = guessSoFar.toString();
         document.getElementById("guessSoFar").innerHTML = y;
         i++;
     }
-
+    /* user lost the game */
     if (i === totalNumGuessAllowed) {
         document.getElementById("loseNum").innerHTML = ++loseNum;
         initialize();
